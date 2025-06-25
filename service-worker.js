483e6222dd3a6480b04c7f73c0ce98d7dcda1789
sateudasyukur <mumubacco1@gmail.com>
2025-06-25 17:35:17 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,46 @@
-// Dummy service worker
-self.addEventListener('fetch', () => {});
+
+const CACHE_NAME = "toko-cache-v1";
+const urlsToCache = [
+  "/toko-sederhana-pwa/",
+  "/toko-sederhana-pwa/index.html",
+  "/toko-sederhana-pwa/icon-192.png",
+  "/toko-sederhana-pwa/icon-512.png",
+  "/toko-sederhana-pwa/manifest.json",
+  "/toko-sederhana-pwa/service-worker.js",
+  // Tambahkan file HTML lainnya jika diperlukan
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
